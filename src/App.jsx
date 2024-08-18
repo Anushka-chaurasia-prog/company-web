f5708cac889da1f4f30f29daaad0db96e83a12a9
Anushkanya <123351032+Anushkanya@users.noreply.github.com>
2024-08-18 09:19:45 +0530

    Normalize line endings

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,18 @@ import Care from './components/careSection/Care'
 import Hero from './components/heroSection/Hero'
 import How from './components/howSection/How'
 import Navbar from './components/navbar/Navbar'
+import Questions from './components/Questions'
+import MpbilePage from './components/MobilePage'
+import HEROSECTION from './components/HEROSECTION'
+// import HealthAdd from './components/HealthAdd'
+import Images from './components/Images'
 import Track from './components/trackSection/Track'
 import Spacer from './components/utils/Spacer'
 import './index.css'
 import { Box } from '@mui/material'
+// import HealthAdvice from './components/HealthAdvice'
+import HealthRecords from './components/HealthRecord'
+
 function App() {
 
 
@@ -30,6 +38,16 @@ function App() {
 
 
       </Box>
+     
+      <Images/>
+      <HEROSECTION/>
+      <HealthRecords/>
+      <MpbilePage/>
+
+      {/* <HealthAdd/> */}
+      <Questions/>
+    
+      
 
 
     </>
